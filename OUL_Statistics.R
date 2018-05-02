7aeff5751c888733091c523a49d65bfbe76cd377
San Miguel <tishas@amgen.com>
2018-05-02 10:44:55 -0700

    statistics updated

---
+++ OUL_Statistics.R
@@ -0,0 +1,72 @@
+library(tidyverse)
+
+#Student Informaion: Who failed and who didn't
+
+studentInfo <- read_csv("C:/Users/tishas/Desktop/personal/Springboard/exercises/Springboard_Capstone_Project/datasets/studentInfo.csv")
+sum(is.na(studentInfo$final_result))
+#drop gender, region, education, band, age, attempts, credits, disability
+studentInfo1 <- studentInfo %>% select(code_module, code_presentation, id_student, final_result)
+#change code_presentation
+studentInfo2 <- studentInfo1 %>%
+  mutate(code_presentation = gsub(pattern = "B", replacement = "February", x = code_presentation),
+         code_presentation = gsub(pattern =  "J", replacement = "October", x = code_presentation))
+studentInfo2 %>% View
+
+#Would like to be ale to use summarise for the following
+
+studentInfo3 <- filter(studentInfo2, final_result == "Distinction") %>% 
+  summarise(sum_pass = sum(final_result == "Distinction"))
+studentInfo3 %>% glimpse
+#3,024 students received Distinction
+studentInfo4 <- filter(studentInfo2, final_result == "Pass") %>% 
+  summarise(sum_pass = sum(final_result == "Pass"))
+studentInfo4 %>% glimpse
+#12,361 students passed
+studentInfo5 <- filter(studentInfo2, final_result == "Fail") %>% 
+  summarise(sum_pass = sum(final_result == "Fail"))
+studentInfo5 %>% glimpse
+#7,052 students failed
+studentInfo6 <- filter(studentInfo2, final_result == "Withdrawn") %>% 
+  summarise(sum_pass = sum(final_result == "Withdrawn"))
+studentInfo6 %>% glimpse
+#10,156 students withdrew
+
+#Distribution of Scores 
+
+studentAssessment <- read_csv("C:/Users/tishas/Desktop/personal/Springboard/exercises/Springboard_Capstone_Project/datasets/studentAssessment.csv")
+sum(is.na(studentAssessment))
+#remove date and banked 
+studentAssessment1 <- studentAssessment %>% select(-date_submitted, -is_banked)
+hist(studentAssessment$score)
+studentAssessment1 %>% glimpse
+studentAssessment1 %>% filter(score <= 40) %>% summarise(sum_below_40 = sum(score <= 40))
+#9544 students did not make the 40% cutoff
+
+#How often students use the modules
+
+studentVle <- read_csv("C:/Users/tishas/Desktop/personal/Springboard/exercises/Springboard_Capstone_Project/datasets/studentVle.csv")
+studentVle %>% glimpse
+sum(is.na(studentVle))
+studentVle1 <- studentVle %>% select(-date)
+studentVle1 %>% dim
+studentVle1 %>% arrange(sum_click)
+plot(studentVle$sum_click)
+
+#Types of assessment used and when
+
+assessments <- read_csv("C:/Users/tishas/Desktop/personal/Springboard/exercises/Springboard_Capstone_Project/datasets/assessments.csv")
+assessments %>% glimpse
+sum(is.na(assessments))
+assessments1 <- assessments %>%
+  mutate(code_presentation = gsub(pattern = "B", replacement = "February", x = code_presentation),
+         code_presentation = gsub(pattern =  "J", replacement = "October", x = code_presentation),
+         assessment_type = gsub(pattern = "TMA", replacement = "Tutor Marked Assessment", x = assessment_type),
+         assessment_type = gsub(pattern = "CMA", replacement = "Computer Marked Assessment", x = assessment_type),
+         assessment_type = gsub(pattern = "Exam", replacement = "Final Exam", x = assessment_type))
+assessments1 %>% glimpse
+
+
+
+
+
+
